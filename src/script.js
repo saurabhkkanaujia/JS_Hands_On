47a9fd5e349c1109d4e25b41f833224a3e834a14
saurabhkkanaujia <saurabhkumarkanaujia@cedcoss.com>
2022-02-05 15:27:33 +0530

    Iterate with JavaScript For Loops

--- src/script.js
+++ src/script.js
@@ -1,9 +1,15 @@
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
 // Setup
 var myArray = [];
-var i = 0 ;
-while (i < 5) {
+
+for (var i = 1; i < 6; i++) {
   myArray.push(i);
-  i++;
 }
 
 // Only change code below this line.
