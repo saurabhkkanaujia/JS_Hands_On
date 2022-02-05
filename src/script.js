3f3afb91392184a06053aecf23ca9f74ffaf89db
saurabhkkanaujia <saurabhkumarkanaujia@cedcoss.com>
2022-02-05 14:01:48 +0530

    Golf Code

--- src/script.js
+++ src/script.js
@@ -1,18 +1,27 @@
-function testSize(num) {
-    // Only change code below this line
-    if (num < 5) {
-     return "Tiny";
-  } else if (num < 10) {
-     return "Small";
-  } else if (num < 15) {
-     return "Medium";
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+     return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+     return "Eagle";
+  } else if (strokes == par - 1) {
+     return "Birdie";
   
-  } else if (num < 20) {
-     return "Large";
-  } else {
-     return "Huge";
+  } else if (strokes == par) {
+     return "Par";
   }
-    // Only change code above this line
+  else if (strokes == par + 1) {
+     return "Bogey";
   }
-   
-  testSize(7);
+  else if (strokes == par + 2) {
+     return "Double Bogey";
+  }
+  else if (strokes >= par + 3) {
+     return "Go Home!";
+  }
+  // Only change code above this line
+}
+
+golfScore(5, 4);
