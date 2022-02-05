a3cb970d70d09a3ea3a5e3a2a27a3d046859accd
saurabhkkanaujia <saurabhkumarkanaujia@cedcoss.com>
2022-02-05 12:05:54 +0530

    Appending Variables to Strings

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
-// Only change code below this line
-const myName = "Saurabh Kumar Kanaujia";
-const myStr = "My name is "+myName+" and I am well!";
+// Change code below this line
+const someAdjective = "aaa";
+let myStr = "Learning to code is ";
+myStr+=someAdjective;
