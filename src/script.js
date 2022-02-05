b4295d9fbcaef74620410d52e1c1bc5d28f183d0
saurabhkkanaujia <saurabhkumarkanaujia@cedcoss.com>
2022-02-05 13:51:49 +0530

    function testSize(num) {

--- src/script.js
+++ src/script.js
@@ -1,14 +1,18 @@
-function orderMyLogic(val) {
+function testSize(num) {
+    // Only change code below this line
+    if (num < 5) {
+     return "Tiny";
+  } else if (num < 10) {
+     return "Small";
+  } else if (num < 15) {
+     return "Medium";
   
-    if (val < 5) {
-      return "Less than 5"; 
-    } 
-    else if (val < 10) {
-      return "Less than 10";
-    
-    } else {
-      return "Greater than or equal to 10";
-    }
+  } else if (num < 20) {
+     return "Large";
+  } else {
+     return "Huge";
+  }
+    // Only change code above this line
   }
   
-  orderMyLogic(4);
+  testSize(7);
