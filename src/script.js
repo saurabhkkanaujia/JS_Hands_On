6c5cce02ffb230b9e31bb2a3f2eee34ce673093d
saurabhkkanaujia <saurabhkumarkanaujia@cedcoss.com>
2022-02-05 14:39:56 +0530

    Returning Boolean Values from Functions

--- src/script.js
+++ src/script.js
@@ -1,26 +1,9 @@
-function chainToSwitch(val) {
-    let answer = "";
-    // Only change code below this line
-    switch(val){
-      case "bob":
-        answer = "Marley";
-        break;
-      case 42:
-        answer = "The Answer";
-        break;
-      case 1:
-        answer = "There is no #1";
-        break;
-      case 99:
-        answer = "Missed me by this much!";
-        break;
-      case 7:
-        answer = "Ate Nine";
-        break;
-      
-    }
-    // Only change code above this line
-    return answer;
-  }
+function isLess(a, b) {
+  // Only change code below this line
   
-  chainToSwitch(7);
+    return a<b;
+  
+  // Only change code above this line
+}
+
+isLess(10, 15);
